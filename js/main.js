da66435f0222b59067358f7d92334eedcfe7b369
mariangelauribe <marita.91.up@gmail.com1>
2017-01-16 09:44:13 -0500

    agregando trabajo

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("form-signin-heading").innerHTML="Por favor inicie sesión";
+document.getElementById("inputEmail").placeholder="Por favor ingrese su correo";
+document.getElementById("inputPassword").placeholder="Contraseña";
+document.querySelector("span").innerHTML="Recordar datos";
+document.querySelector("button").innerHTML="Iniciar sesión";
